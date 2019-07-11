34821697fd97fba93ac51f3ae05ed19e257f8264
heatheraislinn <heatheraislinn@gmail.com>
2019-07-11 12:22:58 -0400

    Personal side codebase

---
+++ js/slide.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $("#alButton").click(function () {
+        $("#alExpanded").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#whButton").click(function () {
+        $("#whExpanded").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#rButton").click(function () {
+        $("#rExpanded").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#dhmButton").click(function () {
+        $("#dhmExpanded").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#itsButton").click(function () {
+        $("#itsExpanded").slideToggle("slow");
+    });
+});
